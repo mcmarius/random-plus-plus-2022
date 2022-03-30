c662c5c592363ac65caab085d865a6869b90cdcb
mcmarius <m.c.marius@outlook.com>
2022-03-30 11:53:05 +0300

    Laborator 7

--- main.cpp
+++ main.cpp
@@ -1,41 +1,48 @@
 
 #include <string>
-#include <ostream>
+#include <iostream>
 #include <memory>
-#include "Incarcator.h"
-#include "Magazin.h"
+#include <utility>
+#include <vector>
+//#include "Incarcator.h"
+//#include "Magazin.h"
 
 //#pragma pack(1)
 
-using namespace std::string_literals;
+//using namespace std::string_literals;
 
-class Vanzator {
-    std::string nume;
-    int vechime;
-public:
-    Vanzator(const std::string &nume, int vechime);
-    friend std::ostream &operator<<(std::ostream &os, const Vanzator &vanzator);
-};
+//struct asd1 {
+//    int x;
+//    char z;
+//};
 
-Vanzator::Vanzator(const std::string &nume, int vechime) : nume(nume), vechime(vechime) {}
-
-std::ostream &operator<<(std::ostream &os, const Vanzator &vanzator) {
-    os << "nume: " << vanzator.nume << " vechime: " << vanzator.vechime;
-    return os;
-}
+//class Vanzator {
+//    std::string nume;
+//    int vechime;
+//public:
+//    Vanzator(const std::string &nume, int vechime);
+//    friend std::ostream &operator<<(std::ostream &os, const Vanzator &vanzator);
+//};
+//
+//Vanzator::Vanzator(const std::string &nume, int vechime) : nume(nume), vechime(vechime) {}
+//
+//std::ostream &operator<<(std::ostream &os, const Vanzator &vanzator) {
+//    os << "nume: " << vanzator.nume << " vechime: " << vanzator.vechime;
+//    return os;
+//}
 
 //class Priza {};
 
-void f([[maybe_unused]]Magazin m) {
-    std::cout << "mag\n";
-}
-void f([[maybe_unused]]Accesoriu s) {
-    std::cout << "acc\n";
-}
-
-void g(Accesoriu) {
-    std::cout << "g\n";
-}
+//void f([[maybe_unused]]Magazin m) {
+//    std::cout << "mag\n";
+//}
+//void f([[maybe_unused]]Accesoriu s) {
+//    std::cout << "acc\n";
+//}
+//
+//void g(Accesoriu) {
+//    std::cout << "g\n";
+//}
 
 class baza1 {
     char x;
@@ -45,17 +52,50 @@ class derivata1 : public baza1 {
     short u;
 };
 
+class baza3_0 {
+public:
+    enum tip {der3_0, der3_01};
+    baza3_0() {
+        std::cout << "constr baza3_0\n";
+    }
+    virtual ~baza3_0() {
+        std::cout << "destr baza3_0\n";
+    }
+};
+class derivata3_0 : public baza3_0 {
+public:
+    derivata3_0() {
+        std::cout << "constr der3_0\n";
+    }
+    ~derivata3_0() override {
+        std::cout << "destr der3_0\n";
+    }
+    void g() {
+        std::cout << "g der3_0\n";
+    }
+};
+class derivata3_01 : public baza3_0 {
+    int x;
+public:
+    derivata3_01() : x{5} {
+        std::cout << "constr der3_01\n";
+    }
+    ~derivata3_01() override {
+        std::cout << "destr der3_01\n";
+    }
+    void f() {
+        std::cout << x << " f der3_01\n";
+    }
+};
+
 class baza2 {
 protected:
     int x = 5;
     baza2(const baza2&) = default;
     baza2& operator=(const baza2&) = default;
+    virtual void afisare(std::ostream& os) const = 0;
 public:
     friend std::ostream &operator<<(std::ostream &os, const baza2 &baza2);
-    virtual void afis(std::ostream& os) const {
-        auto& baza2 = *this;
-        os << "x: " << baza2.x;
-    }
 
     baza2() {
         std::cout << "constr baza2\n";
@@ -67,22 +107,33 @@ public:
         std::cout << "f b2\n";
     }
     virtual void g();
-    virtual std::shared_ptr<baza2> clone() = 0;
+    virtual std::shared_ptr<baza2> clone() const = 0;
 };
+
+void baza2::afisare(std::ostream &os) const {
+    auto& baza2 = *this;
+    os << "x: " << baza2.x;
+}
 
 void baza2::g() {
     std::cout << "g b2\n";
 }
 
 std::ostream &operator<<(std::ostream &os, const baza2 &baza2) {
-    baza2.afis(os);
+    baza2.afisare(os);
     return os;
 }
 
 class derivata21 : public baza2 {
+    int z{5};
 public:
-    std::shared_ptr<baza2> clone() override {
+    [[nodiscard]] std::shared_ptr<baza2> clone() const override {
         return std::make_shared <derivata21>(*this);
+    }
+
+    void afisare(std::ostream &os) const override {
+        baza2::afisare(os);
+        os << z << "\n";
     }
 
     void g() override {
@@ -104,10 +155,16 @@ public:
 
 class derivata22 : public baza2 {
 public:
-    std::shared_ptr<baza2> clone() override {
+    [[nodiscard]] std::shared_ptr<baza2> clone() const override {
         return std::make_shared <derivata22>(*this);
     }
 
+protected:
+    void afisare(std::ostream &os) const override {
+        baza2::afisare(os);
+    }
+
+public:
     void g() override {
         baza2::g();
         std::cout << "g d22\n";
@@ -125,16 +182,17 @@ public:
     }
 };
 
-class baza3 {
+class baza3_1 {
 public:
-    baza3() {
-        std::cout << "constr baza3\n";
+    baza3_1() {
+        std::cout << "constr baza3_1\n";
     }
-    ~baza3() {
-        std::cout << "destr baza3\n";
+    ~baza3_1() {
+        std::cout << "destr baza3_1\n";
     }
 };
-class derivata3 : public baza3 {
+
+class derivata3 : public baza3_1 {
 public:
     derivata3() {
         std::cout << "constr der3\n";
@@ -147,6 +205,11 @@ public:
 class colectie {
     std::vector<std::shared_ptr<baza2>> chestii;
 public:
+    colectie(std::vector <std::shared_ptr <baza2>> chestii) : chestii(std::move(chestii)) {}
+
+    void adauga(baza2& ob) {
+        chestii.emplace_back(ob.clone());
+    }
     colectie(const colectie& other) {
         for(const auto& chestie : other.chestii)
             chestii.push_back(chestie->clone());
@@ -168,10 +231,81 @@ public:
     }
 };
 
+class my_exceptie : public std::runtime_error {
+public:
+    explicit my_exceptie(const std::string &arg) : runtime_error(arg) {}
+};
+
+class eroare_server : public my_exceptie {
+public:
+    explicit eroare_server(const std::string &arg) : my_exceptie(arg) {}
+};
+
+class eroare_client : public my_exceptie {
+public:
+    explicit eroare_client(const std::string &arg) : my_exceptie(arg) {}
+};
+
+void f2() {
+    std::cout << "f2 inainte\n";
+    throw eroare_client("eroare f2");
+    std::cout << "f2 dupa\n";
+}
+
+void f1() {
+    std::cout << "f1 inainte\n";
+    try {
+        f2();
+        throw eroare_server{"hopa"};
+    } catch (my_exceptie& err) {
+        std::cout << "catch logic err\n" << err.what();
+    }
+    std::cout << "f1 dupa\n";
+}
+
 int main() {
+    //f1();
+    try {
+        std::cout << "inainte throw main" << "\n";
+        f1();
+        std::cout << "dupa throw main" << "\n";
+    } catch (std::runtime_error& err) {
+        std::cout << err.what() << "\n";
+    }
+    colectie c1({std::make_shared <derivata21>(), std::make_shared <derivata22>()});
+    c1.fa_de_toate();
+    derivata21 d1;
+    c1.adauga(d1);
+    c1.fa_de_toate();
+
+    std::cout << sizeof(baza3_0) << "\n";
+    std::cout << sizeof(baza3_1) << "\n";
+    baza3_1* b1 = new derivata3;
+    delete b1;
+    baza3_0* b2 = new derivata3_0;
+//    auto& der = static_cast<derivata3_01&>(*b2);
+//    der.f();
+    try {
+        auto& der = dynamic_cast<derivata3_0&>(*b2);
+        new derivata3_0(der);
+        der.g();
+    } catch (std::bad_cast& err) {
+        std::cout << err.what() << "\n";
+    }
+    auto* der2 = dynamic_cast<derivata3_01*>(b2);
+    if(der2 != nullptr) {
+        der2->f();
+    }
+    else {
+        std::cout << "nu a reusit conversia cu pointer\n";
+    }
+    delete b2;
+//    baza3_1 b1, b2;
+//    std::cout << &b1 << " " << &b2 << "\n";
+//    std::cout << sizeof(asd1) << "\n";
 //    baza2* b2 = new baza2[2];
 //    delete[] b2;
-//    baza3* b3 = new baza3;
+//    baza3_1* b3 = new baza3_1;
 //    delete b3;
 //    derivata21* d2 = new derivata21;
 //    delete d2;
@@ -187,7 +321,7 @@ int main() {
     auto copie = b21->clone();
     copie->f();
 //    delete b21;
-    baza3* b31 = new derivata3;
+    baza3_1* b31 = new derivata3;
     delete b31;
 
     return 0;
@@ -195,13 +329,13 @@ int main() {
     std::cout << "size derivata1: " << sizeof(derivata1) << "\n";
     std::cout << "size baza2: " << sizeof(baza2) << "\n";
     std::cout << "size derivata21: " << sizeof(derivata21) << "\n";
-    Vanzator v{"asd", 3};
-    std::cout<<v << "\n";
-    f(Accesoriu{"asd"s});
-    f(Magazin{"asd"s});
-    g({"asd", 23});
+//    Vanzator v{"asd", 3};
+//    std::cout<<v << "\n";
+//    f(Accesoriu{"asd"s});
+//    f(Magazin{"asd"s});
+//    g({"asd", 23});
 //    Priza p;
-    Incarcator usb{"asd"s, 123};
+//    Incarcator usb{"asd"s, 123};
 //    Magazin m{"asd"s} ;
 //    m.modifica(m);
     return 0;
